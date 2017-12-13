9cbdf566a69a927741255b451d0210a1471d5ae8
usr <usr@note>
2017-12-13 01:55:59 -0300

    refactoring

--- __init__.py
+++ __init__.py
@@ -22,14 +22,3 @@ def addforEachMethodToTuples(propertyName='forEach'):
 
 def addColumnPropertyToLists():
   blackMagic.proxy_builtin( list )['column'] = column
-
-if __name__ == '__main__':
-  
-  def cmd(*args):
-    return ';'.join(args.forEach.str())
-  
-  addforEachMethodToTuples()
-  print(cmd(2,3,4,5))
-  
-  pass
-
